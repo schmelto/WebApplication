24724a3efb4e2f72d3a1c63e2887d408f105d2c4
schmelto <tom.schmelzer@web.de>
2020-09-15 22:05:09 +0200

    add PaymentDetailContext : DbContext

--- WebApplication/Models/PaymentDetail.cs
+++ WebApplication/Models/PaymentDetail.cs
@@ -1,25 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class PaymentDetail
+namespace WebApplication.Models
 {
-    [Key]
-    public int PMId { get; set; }
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
 
-    [Required]
-    [Column(TypeName = "nvarchar(100)")]
-    public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
 
-    [Required]
-    [Column(TypeName = "varchar(16)")]
-    public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
 
-    [Required]
-    [Column(TypeName = "varchar(5)")]
-    public string ExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
 
-    [Required]
-    [Column(TypeName = "varchar(3)")]
-    public string CVV { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV { get; set; }
+    }
 }
